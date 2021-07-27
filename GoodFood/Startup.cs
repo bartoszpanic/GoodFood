33c604fe6270501819a6718700c50b24ac049082
Bartosz Panic <bartosz.panic@gmail.com>
2021-07-27 18:25:37 +0200

    Adding log from method request time

--- GoodFood/Startup.cs
+++ GoodFood/Startup.cs
@@ -41,6 +41,8 @@ namespace GoodFood
             services.AddAutoMapper(this.GetType().Assembly);
 
             services.AddSwaggerGen();
+
+            services.AddScoped <RequestTimeMiddleware>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +56,7 @@ namespace GoodFood
             }
 
             app.UseMiddleware<ErrorHandlingMiddleware>();
+            app.UseMiddleware<RequestTimeMiddleware>();
             app.UseHttpsRedirection();
 
 
