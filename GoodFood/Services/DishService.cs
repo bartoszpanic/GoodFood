adcc957fb3207952197eeeeea3fa7156eb749c54
Bartosz Panic <bartosz.panic@gmail.com>
2021-07-30 11:37:39 +0200

    Add dish Get method

--- GoodFood/Services/DishService.cs
+++ GoodFood/Services/DishService.cs
@@ -2,6 +2,7 @@
 using GoodFood.Entities;
 using GoodFood.Exceptions;
 using GoodFood.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,11 +31,47 @@ namespace GoodFood.Services
             var dishEntity = _mapper.Map<Dish>(dto);
 
             dishEntity.RestaurantId = restaurantId;
-            
+
             _db.Dishes.Add(dishEntity);
             _db.SaveChanges();
 
             return dishEntity.Id;
+        }
+
+        public DishDto GetById(int restaurantId, int dishId)
+        {
+            var restaurant = _db.Restaurants.FirstOrDefault(r => r.Id == restaurantId);
+
+            if (restaurant is null)
+            {
+                throw new NotFoundException("Restaurant not found");
+            }
+
+            var dish = _db.Dishes.FirstOrDefault(d => d.Id == dishId);
+
+            if (dish is null || dish.RestaurantId != restaurantId)
+            {
+                throw new NotFoundException("Dish not found");
+            }
+
+            var dishDto = _mapper.Map<DishDto>(dish);
+            return dishDto;
+        }
+
+        public List<DishDto> GetAll(int restaurantId)
+        {
+            var restaurant = _db
+                .Restaurants
+                .Include(r => r.Dishes)
+                .FirstOrDefault(r => r.Id == restaurantId);
+
+            if (restaurant is null)
+            {
+                throw new NotFoundException("Restaurant not found");
+            }
+
+            var dishDtos = _mapper.Map<List<DishDto>>(restaurant.Dishes);
+            return dishDtos;
         }
     }
 }
--- GoodFood/Services/IDishService.cs
+++ GoodFood/Services/IDishService.cs
@@ -9,5 +9,7 @@ namespace GoodFood.Services
     public interface IDishService
     {
         int Create(int restaurantId, CreateDishDto dto);
+        DishDto GetById(int restaurantId, int dishId);
+        List<DishDto> GetAll(int restaurantId);
     }
 }
