dbab8a21a8005366b55274cacd56aeca6063813a
Bartosz Panic <bartosz.panic@gmail.com>
2021-08-03 16:49:05 +0200

    Problem with authorize

---
+++ GoodFood/Authorization/ResourceOperationRequirement.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace GoodFood.Authorization
+{
+    public enum Operation
+    {
+        Create,
+        Read,
+        Update,
+        Delete
+    }
+    public class ResourceOperationRequirement : IAuthorizationRequirement
+    {
+        public ResourceOperationRequirement(Operation operation)
+        {
+            Operation = operation;
+        }
+        public Operation Operation { get; }
+    }
+}
---
+++ GoodFood/Exceptions/ForbidException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodFood.Exceptions
+{
+    public class ForbidException : Exception
+    {
+    }
+}
